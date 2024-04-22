b93c525d61604d1b459531dd71e875b1f4c520d0
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-04-22 11:23:36 -0300

    Lista 1 - Ex11

--- Program.cs
+++ Program.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lista1Ex10
+namespace Lista1Ex11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double cotaçaoDolar;
-            double ValorDolar;
-            double valorReais;
-            
-            
-            Console.Write("Digite o valor da cotação em dolar: ");
-            cotaçaoDolar = double.Parse(Console.ReadLine());
 
-            Console.Write("Digite a quatidade de doláres: ");
-            ValorDolar = double.Parse(Console.ReadLine());
+            double x;
+            double y;
+            double ResultadoPotencia;
 
-            valorReais = ValorDolar * cotaçaoDolar;
+            Console.Write("Digite o valor de x para calcular sua potência:  ");
+            x = double.Parse(Console.ReadLine());
 
-            Console.Write("O valor da quantidade de dólar referente a contação informada equivale em reais: ");
-            Console.WriteLine(valorReais);
+            Console.Write("Digite o valor de y para ser o expoente:  ");
+            y = double.Parse(Console.ReadLine());
 
+            ResultadoPotencia = Math.Pow(x, y);
+
+            Console.Write("O Resultado da Potência é:  ");
+            Console.WriteLine(ResultadoPotencia);
 
 
 
