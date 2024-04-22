50896855dd09c53d0ef2f73365460e64e0215040
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-04-22 11:23:02 -0300

    Lista 1 - Ex10

--- Program.cs
+++ Program.cs
@@ -4,24 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lista1Ex9
+namespace Lista1Ex10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double cotaçaoDolar;
+            double ValorDolar;
+            double valorReais;
+            
+            
+            Console.Write("Digite o valor da cotação em dolar: ");
+            cotaçaoDolar = double.Parse(Console.ReadLine());
 
-            double diamentro;
-            double area;
+            Console.Write("Digite a quatidade de doláres: ");
+            ValorDolar = double.Parse(Console.ReadLine());
 
-            Console.Write("Digite o valor do diâmentro do Círculo:  ");
-            diamentro = double.Parse(Console.ReadLine());
+            valorReais = ValorDolar * cotaçaoDolar;
+
+            Console.Write("O valor da quantidade de dólar referente a contação informada equivale em reais: ");
+            Console.WriteLine(valorReais);
 
 
-            area = Math.Pow( diamentro/2,2) * 3.14;
 
-            Console.Write("O valor da area do círculo é:  ");
-            Console.WriteLine(area);
+
         }
     }
 }
