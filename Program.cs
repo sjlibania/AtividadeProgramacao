a657f78104b00d7f90198a21bdc033320e858ae1
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-04-22 11:24:12 -0300

    Lista1 - Ex12

--- Program.cs
+++ Program.cs
@@ -4,27 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lista1Ex11
+namespace Lista1Ex12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int P1;
+            int P2;
+            int P3; 
+            int P4;
+            int P5;
+            double ValorSomaProd;
+            double ValorPag;
+            double troco;
 
-            double x;
-            double y;
-            double ResultadoPotencia;
+            Console.Write("Digite o valor do primeiro produto: ");
+            P1 = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o valor de x para calcular sua potência:  ");
-            x = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do segundo produto: ");
+            P2 = int.Parse(Console.ReadLine());
 
-            Console.Write("Digite o valor de y para ser o expoente:  ");
-            y = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do terceiro produto: ");
+            P3 = int.Parse(Console.ReadLine());
 
-            ResultadoPotencia = Math.Pow(x, y);
+            Console.Write("Digite o valor do quarto produto: ");
+            P4 = int.Parse(Console.ReadLine());
 
-            Console.Write("O Resultado da Potência é:  ");
-            Console.WriteLine(ResultadoPotencia);
+            Console.Write("Digite o valor do quinto produto: ");
+            P5 = int.Parse(Console.ReadLine());
+
+            ValorSomaProd = P1 + P2 + P3 + P4 + P5;
+
+            Console.Write("O valor da soma dos produtos é:  ");
+            Console.WriteLine(ValorSomaProd);
+
+            Console.Write("Digite o valor para pagamento:  ");
+            ValorPag = double.Parse(Console.ReadLine());
+
+            troco = ValorPag - ValorSomaProd;
+
+            Console.WriteLine("Este é o seu troco");
+            Console.WriteLine(troco);
+
+            
+
+
 
 
 
