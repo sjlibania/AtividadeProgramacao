f958187fca99a2e1519b583f5c70564205b0341e
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-04-22 11:20:20 -0300

    Lista1 - Ex6

--- Program.cs
+++ Program.cs
@@ -4,36 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lista1Ex5
+namespace Lista1Ex6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double valor1;
+            double valor2;
+            double mediageometrica;
 
-            int a;
-            int b;
-            int c;
-            int d;
-            int mediaaritmetica;
 
             Console.Write("Digite o primeiro valor: ");
-            a = int.Parse(Console.ReadLine());
+            valor1 = double.Parse(Console.ReadLine());
 
             Console.Write("Digite o segundo valor: ");
-            b = int.Parse(Console.ReadLine());
+            valor2 = double.Parse(Console.ReadLine());
 
-            Console.Write("Digite o terceiro valor: ");
-            c = int.Parse(Console.ReadLine());
+            mediageometrica = Math.Sqrt(valor1 * valor2);
 
-            Console.Write("Digite o quarto valor:  ");
-            d = int.Parse(Console.ReadLine());
+            Console.Write("A média geométrica dos 2 valores informados é:  ");
+            Console.WriteLine(mediageometrica);
 
-            mediaaritmetica = (a + b + c + d)/4;
-            
 
-            Console.Write("A Média Aritmética dos 4 valores informados é: ");
-            Console.WriteLine(mediaaritmetica);
+
 
 
         }
