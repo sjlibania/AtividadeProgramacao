4e7cebe910d5c774250ec785e9c978bfd24b16b3
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-04-22 11:14:02 -0300

    Lista1 - Ex1

---
+++ Lista1Ex1 - Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+
+            Console.Write("Digite o valor da Base do retângulo: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura do retângulo: ");
+            a = int.Parse(Console.ReadLine());
+
+            area = b * a;
+
+            Console.Write("O valor da area do retângulo é: ");
+            Console.WriteLine(area);
+
+        }
+        }
+}
